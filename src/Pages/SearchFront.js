6a6d58cf96f4c7175d955496b58e73ceb8785774
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-29 21:05:20 +0000

    finish the basic version of the search algorithem

--- src/Pages/SearchFront.js
+++ src/Pages/SearchFront.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchFront.css'
+import { Users } from '../data/users'
 
 export const SearchFront = () => {
+  const [input, setInput] = useState('');
+  // console.log(Users.filter(user=>user.first_name.toLowerCase().includes("fe")))
   return (
-    <div className="search-front">SearchFront</div>
+    <div className="search-front">
+      <h1>SearchFront</h1>
+      <div>
+        <input type="text" placeholder="Search ..." onChange={(e) => setInput(e.target.value)} />
+
+        <ul>
+          {Users.filter(user => user.first_name.toLowerCase().includes(input))
+            .map((user) =>
+              <li key={user.id}>{user.first_name}</li>
+            )}
+        </ul>
+      </div>
+    </div>
   );
 }
