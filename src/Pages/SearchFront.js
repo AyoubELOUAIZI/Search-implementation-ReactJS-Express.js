b654caf284ec2d6a863ceef6b9f4a8e5dc0aa3d3
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-29 21:55:38 +0000

    adding search with all atributes

--- src/Pages/SearchFront.js
+++ src/Pages/SearchFront.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
 import './SearchFront.css'
 import { Users } from '../data/users'
+import Table from '../components/Table';
 
 export const SearchFront = () => {
   const [input, setInput] = useState('');
+  //first v1
   // console.log(Users.filter(user=>user.first_name.toLowerCase().includes("fe")))
+  // return (
+  //   <div className="search-front">
+  //     <h1>SearchFront</h1>
+  //     <div>
+  //       <input type="text" placeholder="Search ..." onChange={(e) => setInput(e.target.value)} />
+
+  //       <ul>
+  //         {Users.filter(user => user.first_name.toLowerCase().includes(input))
+  //           .map((user) =>
+  //             <li key={user.id}>{user.first_name}</li>
+  //           )}
+  //       </ul>
+  //     </div>
+  //   </div>
+  // );
+
+  //v2
   return (
     <div className="search-front">
       <h1>SearchFront</h1>
       <div>
         <input type="text" placeholder="Search ..." onChange={(e) => setInput(e.target.value)} />
 
-        <ul>
-          {Users.filter(user => user.first_name.toLowerCase().includes(input))
-            .map((user) =>
-              <li key={user.id}>{user.first_name}</li>
-            )}
-        </ul>
+
+        <Table Users={Users.filter(user => user.first_name.toLowerCase().includes(input) ||
+          user.last_name.toLowerCase().includes(input) ||
+          user.email.toLowerCase().includes(input) ||
+          user.gender.toLowerCase().includes(input) 
+          )} />
       </div>
     </div>
   );
+
+
 }
---
+++ src/components/Table.js
@@ -0,0 +1,26 @@
+
+import './Table.css'
+const Table = ({ Users }) => {
+    return (
+        <table className="table" border="1">
+            <tbody>
+                <tr>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email Address</th>
+                    <th>Gender</th>
+                </tr>
+                {Users.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.first_name}</td>
+                        <td>{user.last_name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.gender}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
